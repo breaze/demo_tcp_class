aeddeb764f7b2c07f94ceaf7eb609a3e6d194613
breaze <simon.gaviriao@gmail.com>
2025-01-23 17:54:58 -0500

    Uploading client

---
+++ TCPDemoClient/src/main/java/com/breaze/tcpdemoclient/TCPDemoClient.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.breaze.tcpdemoclient;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author breaze
+ */
+public class TCPDemoClient {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type your name");
+        String name = in.nextLine();
+        System.out.println("Type your last name");
+        String lastName = in.nextLine();
+        
+        
+    }
+}
---
+++ TCPDemoClient/src/main/java/com/breaze/tcpdemoclient/networklayer/TCPClient.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.breaze.tcpdemoclient.networklayer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class that handle connection with the server
+ * @author breaze
+ */
+public class TCPClient {
+    private String serverAddress;
+    private int port;
+    private Socket clientSocket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+
+    public TCPClient(String serverAddress, int port) {
+        this.serverAddress = serverAddress;
+        this.port = port;
+    }
+    
+    public void connect() throws IOException{
+        clientSocket = new Socket(serverAddress, port);
+        System.out.println("Connection established");
+        inputStream = new DataInputStream(clientSocket.getInputStream());
+        outputStream = new DataOutputStream(clientSocket.getOutputStream());
+    }
+    
+    public String sendMessage(String name, String lastName){
+        String response = "Error";
+        try {
+            connect();
+            StringBuilder sb = new StringBuilder();
+            sb.append(name).append(":").append(lastName);
+            String message = sb.toString();
+            System.out.println("Sending: "+message);
+            outputStream.writeUTF(message);
+            response = inputStream.readUTF();
+            System.out.println("Response: "+response);
+        } catch (IOException ex) {
+            System.out.println("Client error: "+ex.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return response;
+    }
+    
+    public void closeConnection(){
+        try{
+            if(inputStream != null){
+            inputStream.close();
+            }
+            if(outputStream != null){
+                outputStream.close();
+            }
+            if(clientSocket != null){
+                clientSocket.close();
+            }
+        }catch(IOException ex){
+            System.out.println("Error closing connection: "+ex.getMessage());
+        }
+        
+        
+    }
+    
+    
+    
+     
+    
+    
+}
